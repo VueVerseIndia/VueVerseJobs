95d24e41d18b29999a6249b5e5da8c9fd9fe5289
vvsubash <subash.venkat007@gmail.com>
2026-06-28 12:39:35 +0530

    feat: adding scalar for api documentation

--- vuejobs-india/nuxt.config.ts
+++ vuejobs-india/nuxt.config.ts
@@ -6,9 +6,33 @@ export default defineNuxtConfig({
   runtimeConfig: {
     databaseUrl: "",
   },
+  routeRules: {
+    "/docs/**":{
+      ssr: false,
+      
+    }
+  },
+  modules: ["@scalar/nuxt"],
+  scalar: {
+    spec: {
+      url: "/_openapi.json",
+    },
+  },
+  nitro: {
+    experimental: {
+      openAPI: true,
+    },
+  },
   devtools: { enabled: true },
   css: ["./app/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
+    optimizeDeps: {
+       include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit',
+      ],
+      exclude: ["web-worker"],
+    },
   },
 });
