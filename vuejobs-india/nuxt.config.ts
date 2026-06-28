09548153596ebfe0ce802a10fa71bddaa6f8b3f4
vvsubash <subash.venkat007@gmail.com>
2026-06-28 11:44:26 +0530

    feat: adding drizzle orm and connecting with postgres database

--- vuejobs-india/nuxt.config.ts
+++ vuejobs-india/nuxt.config.ts
@@ -1,14 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from '@tailwindcss/vite'
-
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  css: ['./app/assets/css/main.css'],
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+  compatibilityDate: "2025-07-15",
+  runtimeConfig: {
+    databaseUrl: "",
   },
-})
+  devtools: { enabled: true },
+  css: ["./app/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
