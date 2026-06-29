a9116e0efff05f1bd735a5a9d30297481ddefc5e
venkat <subash.venkat007@gmail.com>
2026-06-29 23:01:52 +0530

    Feature/database setup (#12)
    * feat: adding Dockerfile and docker compose file for spinning up postgresql
    
    * feat: adding drizzle orm and connecting with postgres database
    
    * feat: writing first table and migration
    
    * feat: adding an endpoint to check database health
    
    * feat: adding scalar for api documentation

--- vuejobs-india/nuxt.config.ts
+++ vuejobs-india/nuxt.config.ts
@@ -1,14 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from '@tailwindcss/vite'
-
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  css: ['./app/assets/css/main.css'],
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+  compatibilityDate: "2025-07-15",
+  runtimeConfig: {
+    databaseUrl: "",
   },
-})
+  routeRules: {
+    "/docs/**":{
+      ssr: false,
+      
+    }
+  },
+  modules: ["@scalar/nuxt"],
+  scalar: {
+    spec: {
+      url: "/_openapi.json",
+    },
+  },
+  nitro: {
+    experimental: {
+      openAPI: true,
+    },
+  },
+  devtools: { enabled: true },
+  css: ["./app/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+    optimizeDeps: {
+       include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit',
+      ],
+      exclude: ["web-worker"],
+    },
+  },
+});
